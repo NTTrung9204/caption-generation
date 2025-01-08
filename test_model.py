5e0afe8848e7b748f5a60aba309ae5b4f447d513
Trung Nguyễn <trung9204@gmail.com>
2025-01-08 23:23:54 +0700

    update: test model with list images

--- test_model.py
+++ test_model.py
@@ -1,66 +1,59 @@
 from build_model.model import CaptionGenerationNet
 from build_model.inception_v3_model import InceptionV3Model
 from helper import Helper
-from keras.preprocessing.sequence import pad_sequences # type: ignore
+from keras.preprocessing.sequence import pad_sequences  # type: ignore
 import torch
-import random
-import numpy as np
-
-# random.seed(42)
-# np.random.seed(42)
-# torch.manual_seed(42)
-# torch.cuda.manual_seed_all(42)
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    # image_name = "58363928_6f7074608c.jpg"
-    # image_name = "69189650_6687da7280.jpg"
-    # image_name = "667626_18933d713e.jpg"
-    # image_name = "3000722396_1ae2e976c2.jpg"
-    # image_name = "300148649_72f7f0399c.jpg"
-    image_name = "3004823335_9b82cbd8a7.jpg"
-
-    encoding_images_dict_path = "encoding_images_dict.pkl"
-    captions_pathname = "dataset/captions.txt"
-
     device = torch.device("cuda")
 
     helper_func = Helper(device)
+    extractor = InceptionV3Model(device)
+
+    captions_pathname = "dataset/captions.txt"
+    encoding_images_dict_path = "encoding_images_dict.pkl"
 
     captions = helper_func.load_captions(captions_pathname)
-
     processed_caption = helper_func.captions_processing(captions)
-
     vocab = helper_func.build_vocab(processed_caption, word_count_threshold=10)
     vocab_size = len(vocab)
 
-    model = torch.load("trained_model_v3.pth").to(device)
+    model = torch.load("trained_model_v5.pth").to(device)
     model.eval()
-
-    encoding_images_dict = helper_func.load_features_from_file(encoding_images_dict_path)
-
-    image_input_encoding = torch.tensor(encoding_images_dict[image_name]).to(device)
-
-    input_sequences = "startseq"
-
-    in_seq = [vocab.word_to_index[word] for word in input_sequences.split()]
 
     max_length = helper_func.calculate_max_length(processed_caption)
 
-    print("Max length:", max_length)
+    image_files = [f"actual_test/test_{i}.jpg" for i in range(1, 6)]
 
-    seq_input_encoding = torch.tensor(pad_sequences([in_seq], maxlen=max_length)[0]).to(device)
+    fig, axes = plt.subplots(1, 5, figsize=(20, 5))
+    for i, image_name in enumerate(image_files):
+        image_input_encoding = torch.tensor(extractor.extract_features([image_name])[image_name.split("/")[-1]]).to(device)
 
-    while True:
-        if input_sequences.split()[-1] == "endseq" or len(input_sequences.split()) > max_length:
-            print(input_sequences)
-            break
+        input_sequences = "startseq"
+        in_seq = [vocab.word_to_index[word] for word in input_sequences.split()]
+        seq_input_encoding = torch.tensor(pad_sequences([in_seq], maxlen=max_length)[0]).to(device)
 
-        with torch.no_grad():
-            output = model((image_input_encoding, seq_input_encoding))
-            input_sequences += " "  + vocab.word_vocab[torch.argmax(output).cpu().detach().numpy()]
-            in_seq = [vocab.word_to_index[word] for word in input_sequences.split()]
+        while True:
+            if (
+                input_sequences.split()[-1] == "endseq"
+                or len(input_sequences.split()) > max_length
+            ):
+                break
 
-            seq_input_encoding = torch.tensor(pad_sequences([in_seq], maxlen=29)[0]).to(device)
+            with torch.no_grad():
+                output = model((image_input_encoding, seq_input_encoding))
+                next_word = vocab.word_vocab[torch.argmax(output).cpu().detach().numpy()]
+                input_sequences += " " + next_word
+                in_seq = [vocab.word_to_index[word] for word in input_sequences.split()]
+                seq_input_encoding = torch.tensor(pad_sequences([in_seq], maxlen=max_length)[0]).to(device)
 
+        caption = input_sequences.replace("startseq", "").replace("endseq", "").strip()
 
+        image = plt.imread(image_name)
+        axes[i].imshow(image)
+        axes[i].set_title(caption, fontsize=8)
+        axes[i].axis("off")
 
+    plt.tight_layout()
+    plt.show()
